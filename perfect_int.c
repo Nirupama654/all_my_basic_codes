1b1ea4da54f200ded6f0d8cc69b53d68140a0b22
adityaaddy <aditya.k@students.iiit.ac.in>
2019-04-18 20:13:45 +0530

    2nd summit

--- perfect_int.c
+++ perfect_int.c
@@ -1,22 +1,20 @@
 #include<stdio.h>
 #include<math.h>
-int main(void)
-{
-int a;
-int b=1;
-int c=0;
-printf("please enter a number :\n");
-scanf("%d",&a);
+int main(void){
+	int a;
+	int b=1;
+	int c=0;
+	printf("please enter a number :\n");
+	scanf("%d",&a);
 
-while(b<a){
-if(a%b==0)
-c=c+b;
-b++;
-}
-if(c==a)
-printf("given number is perfect number\n");
-else
-printf("not a perfect number\n");
-return 0;
-
+	while(b<a){
+		if(a%b==0)
+			c=c+b;
+			b++;
+	}	
+	if(c==a)
+		printf("given number is perfect number\n");
+	else
+		printf("not a perfect number\n");
+	return 0;
 }
